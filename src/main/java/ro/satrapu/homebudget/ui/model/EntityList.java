6f4a306f24ea034a498f85b4840d95479bbc1d40
Bogdan Marian <bogdan.marian77@gmail.com>
2012-10-14 22:09:04 +0300

    Updated PrimeFaces to 3.4.1. Trying to fix division by 0 found inside PrimeFaces lazy model data tables. Moved persistence.xml file to appropriate location. Reconfigured logback in order to put old logs in a separate folder than the one used by the current ones.
    --HG--
    rename : src/main/webapp/WEB-INF/persistence.xml => src/main/resources/META-INF/persistence.xml

--- src/main/java/ro/satrapu/homebudget/ui/model/EntityList.java
+++ src/main/java/ro/satrapu/homebudget/ui/model/EntityList.java
@@ -30,12 +30,24 @@ public abstract class EntityList<T extends Entity> {
         @SuppressWarnings("unchecked")
         final Class<T> clazz = entityClass;
         data = new LazyDataModel<T>() {
-
             private static final long serialVersionUID = 1L;
 
             @Override
             public List<T> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
                 return persistenceService.fetch(clazz, first, pageSize);
+            }
+
+            @Override
+            public void setRowIndex(int rowIndex) {
+                /*
+                 * The following is in ancestor (LazyDataModel):
+                 * this.rowIndex = rowIndex == -1 ? rowIndex : (rowIndex % pageSize);
+                 */
+                if (rowIndex == -1 || getPageSize() == 0) {
+                    super.setRowIndex(-1);
+                } else {
+                    super.setRowIndex(rowIndex % getPageSize());
+                }
             }
         };
 
